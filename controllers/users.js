bcce1d505e681055962964a1349a0c4583debc9e
ditabu <dibueno@gmail.com>
2021-11-17 15:45:50 -0800

    sign up and login working

--- controllers/users.js
+++ controllers/users.js
@@ -5,7 +5,8 @@ const { v4: uuidv4 } = require('uuid');
 const S3 = require("aws-sdk/clients/s3");
 const s3 = new S3();
 
-
+const BUCKET_NAME = process.env.BUCKET_NAME 
+console.log(BUCKET_NAME, "bucket_name working")
 module.exports = {
   signup,
   login
@@ -19,14 +20,13 @@ async function signup(req, res) {
   const filePath = `${uuidv4()}/${req.file.originalname}`;
   // generate our options object for aws
   const params = {
-    Bucket: process.env.BUCKET_NAME,
+    Bucket: BUCKET_NAME, 
     Key: filePath,
     Body: req.file.buffer,
   };
-
+  console.log(params, "is this working")
   s3.upload(params, async function (err, data) {
     // data -> successful response from aws, the file location will be in data.Location
-    console.log(err, ' <- err from aws, are your keys and bucket correct?')
 
 
     const user = new User({...req.body, photoUrl: data.Location});
